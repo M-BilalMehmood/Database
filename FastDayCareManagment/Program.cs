57970a4dd248729cd77a96a1a3d6dfb86aa19ef7
Muhammad Bilal Mehmood <125654503+M-BilalMehmood@users.noreply.github.com>
2024-05-05 01:06:24 +0500

    Made some corrections

--- FastDayCareManagment/Program.cs
+++ FastDayCareManagment/Program.cs
@@ -11,7 +11,7 @@ namespace FastDayCareManagment
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new AdminProfile());
+            Application.Run(new Login());
         }
     }
 }
--- FastDayCareManagment/obj/Debug/net8.0-windows10.0.17763.0/FastDayCareManagment.AssemblyInfo.cs
+++ FastDayCareManagment/obj/Debug/net8.0-windows10.0.17763.0/FastDayCareManagment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastDayCareManagment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29a4128376ded85edb5e29a6aad6e33215c6208e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastDayCareManagment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastDayCareManagment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
